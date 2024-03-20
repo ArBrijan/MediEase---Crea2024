07f3fc8b9e2be80e90d9cf7761ae7e3096e5e8eb
Brian <chillandpepers2k@gmail.com>
2024-03-20 14:19:15 -0600

    Página de incio parcialmente hecha, sin terminar, 20/03/2024

--- vite-project/src/body.jsx
+++ vite-project/src/body.jsx
@@ -3,8 +3,9 @@ export function Body () {
         <h1>dsdwdwd</h1>
     }
     return(
-        <div className="bg-indigo-500 relative">
-            <img className="w-screen max-h-full" src="./src/imgs/banner1.jpg" alt="Banner"/>
+        <div style={{height: "427px"}}className="relative">
+            <img style={{opacity: "0.5", height: "427px"}} className="w-screen" src="./src/imgs/banner1.jpg" alt="Banner"/>
+            <p  style={{top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} className="absolute text-6xl text-black-600">¡Tu salud al alcance de un código!</p>
         </div>
     )
 }
