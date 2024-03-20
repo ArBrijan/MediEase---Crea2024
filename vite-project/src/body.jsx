41fa6cef7bc726181f74841d673260284342fae6
Brian <chillandpepers2k@gmail.com>
2024-03-20 13:45:10 -0600

    Esoo

---
+++ vite-project/src/body.jsx
@@ -0,0 +1,10 @@
+export function Body () {
+    const Banner = () => {
+        <h1>dsdwdwd</h1>
+    }
+    return(
+        <div className="bg-indigo-500 relative">
+            <img className="w-screen max-h-full" src="./src/imgs/banner1.jpg" alt="Banner"/>
+        </div>
+    )
+}
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Header } from './header'
+import { Body } from './body'
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<button>Sexo button</button>)
+root.render(
+    <>
+    <Header/>
+    <Body/>
+    </>
+)
