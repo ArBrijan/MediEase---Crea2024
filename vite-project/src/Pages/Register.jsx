f72e32f29b8628aa6181d22d452d7a1cfe83cbd8
Tavods <105887380+Tavods@users.noreply.github.com>
2024-08-26 08:25:44 -0600

    actualizadp Register

--- vite-project/src/Pages/Register.jsx
+++ vite-project/src/Pages/Register.jsx
@@ -24,91 +24,85 @@ export function Register() {
       navigate('/Login');
     })
     .catch(err => console.log(err));
-  }  
+  }
 
   return (
-    <div className="w-full h-screen relative flex items-center justify-center">
-      <div
-        style={{ backgroundColor: "#EEF2F5" }}
-        className="flex items-center justify-between p-[30px] absolute top-0 left-0 w-full"
-      >
+    <div className="w-full h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
+      <div className="absolute top-0 left-0 w-full flex items-center justify-between p-5 bg-white shadow-md">
         <img
-          className="w-[170px]"
+          className="w-40"
           src="./src/assets/newLogo.png"
           alt="LogoEmpresa"
         />
-
-        <Link to="/" className="text-white bg-black p-3 rounded">
+        <Link to="/" className="text-white  bg-black rounded-lg p-3 w-[130px] text-center">
           Regresar
         </Link>
       </div>
-      <div className="w-0 md:w-[500px] h-[500px] border hidden md:flex justify-center items-center">
+
+      <div className="hidden md:flex md:w-[515px] h-[500px] items-center justify-center">
         <img
-          className="w-[450px] h-[495px]"
+          className="rounded-xl shadow-lg"
           src="./src/assets/pills.png"
           alt="Imagen"
         />
       </div>
-      <form onSubmit={handleSubmit} className="w-[500px] h-[500px] p-5 border flex flex-col justify-center">
-        <h1 className="text-3xl font-medium">Regístrate</h1>
-        <br />
+
+      <form onSubmit={handleSubmit} className="bg-white shadow-xl rounded-lg p-10 flex flex-col w-[350px] md:w-[500px]">
+        <h1 className="text-4xl font-semibold text-black mb-8 text-center">
+          Regístrate
+        </h1>
         <input
-          className="border p-2"
+          className="border border-gray-300 rounded-lg p-3 mb-5 focus:outline-none"
           type="text"
           placeholder="Nombre Completo"
           required
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        <br />
         <input
-          className="border p-2"
+          className="border border-gray-300 rounded-lg p-3 mb-5 focus:outline-none"
           type="email"
           placeholder="Correo"
           required
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <br />
         <input
-          className="border p-2"
+          className="border border-gray-300 rounded-lg p-3 mb-5 focus:outline-none"
           type="password"
           placeholder="Contraseña"
           required
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <br />
         <input
-          className="border p-2"
+          className="border border-gray-300 rounded-lg p-3 mb-5 focus:outline-none"
           type="number"
           placeholder="Edad"
           required
           value={age}
           onChange={(e) => setAge(e.target.value)}
         />
-        <br />
         <input
-          className="border p-2"
+          className="border border-gray-300 rounded-lg p-3 mb-8 focus:outline-none"
           type="tel"
           placeholder="Número telefónico"
           required
           value={telefono}
           onChange={(e) => setTelefono(e.target.value)}
         />
-        <br />
-        <div className="flex justify-evenly text-center">
+        <div className="flex justify-between items-center">
           <button
             type="submit"
-            className="text-white bg-black p-3 rounded w-[110px]"
+            className="text-white  bg-black rounded-lg p-3 w-[130px]"
           >
             Registrarse
           </button>
           <Link
             to="/Login"
-            className="text-white bg-black p-3 rounded w-[130px]"
+            className="text-white  bg-black rounded-lg p-3 w-[130px] text-center"
           >
-            Iniciar sesion
+            Iniciar Sesión
           </Link>
         </div>
       </form>
@@ -117,3 +111,4 @@ export function Register() {
 }
 
 export default Register;
+
